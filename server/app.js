dd2637de79f4b3f536f6a3192d958fe1ab6a126b
Hina Dawood <hinadawood@192-168-1-107.tpgi.com.au>
2020-08-17 09:35:50 +1000

    server changes

--- server/app.js
+++ server/app.js
@@ -6,7 +6,9 @@ var app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(require('./src/routes'));
 app.use(express.static(__dirname + '/images'));
 
